6af85e9a9ce6f6bfdc74adb0b19d70c5ca03c311
Lukas <lukas.ancikevicius@gmail.com>
2021-07-09 12:03:10 +0300

    add to cart in progress

--- Routes/cartRoutes.js
+++ Routes/cartRoutes.js
@@ -10,8 +10,21 @@ router.get('/api/shop/cart/:userId', (req, res) => {
 
 // add item to cart
 
-router.post('/api/shop/cart/:userId', (req, res) => {
-  res.json('yo yo add items to cart');
+router.post('/api/shop/cart/:userId', async (req, res) => {
+  console.log('got item to add to cart');
+  console.log(req.body);
+
+  try {
+    // jei jau yra toks cart tai mes norim prideti prie cart objektu
+    const cartExists = Cart.findOne({ userId: req.params.userId });
+    console.log('cartExists', cartExists.mongooseCollection.name);
+    // jei nera tai sukurti nauja
+    const newCart = new Cart({ userId: req.params.userId, cart: [req.body] });
+    const result = await newCart.save();
+    res.json(result);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
