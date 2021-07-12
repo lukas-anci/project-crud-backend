00bdc94e642165d839139d06202fc37911f0a340
Lukas <lukas.ancikevicius@gmail.com>
2021-07-12 11:58:29 +0300

    return cart for specific user

--- Routes/cartRoutes.js
+++ Routes/cartRoutes.js
@@ -4,8 +4,17 @@ const router = express.Router();
 
 // get user cart
 
-router.get('/api/shop/cart/:userId', (req, res) => {
-  res.json(`You want to get cart of a user ${req.params.userId}`);
+router.get('/api/shop/cart/:userId', async (req, res) => {
+  // res.json(`You want to get cart of a user ${req.params.userId}`);
+
+  try {
+    const allCarts = await Cart.find();
+    // find current user cart
+    const currentUserCart = allCarts.find((u) => u.userId == req.params.userId);
+    res.json(currentUserCart.cart);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 // add item to cart
