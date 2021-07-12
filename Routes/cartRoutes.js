5f6ea31a7c7a8b7c525a2add4ed6d358c4afac65
Lukas <lukas.ancikevicius@gmail.com>
2021-07-12 11:21:07 +0300

    quantity ++ on same item to cart

--- Routes/cartRoutes.js
+++ Routes/cartRoutes.js
@@ -12,31 +12,43 @@ router.get('/api/shop/cart/:userId', (req, res) => {
 
 router.post('/api/shop/cart/:userId', async (req, res) => {
   console.log('got item to add to cart');
-  console.log(req.body);
+  // console.log(req.body);
 
   try {
-    // if cart exists
+    //
+    // ar toks krepselis existuoja
     const currentCart = await Cart.findOne({
       userId: req.params.userId,
     }).exec();
-    console.log('currentCart', Boolean(currentCart));
-
+    console.log(' currentCart', Boolean(currentCart));
     // jei jau yra toks cart tai mes norim prideti prie cart objektu
-
     if (!currentCart) {
       const newCart = new Cart({ userId: req.params.userId, cart: [req.body] });
       const result = await newCart.save();
-      res.json({ msg: 'cart created', result });
+      res.json({ msg: 'created a cart', result });
     } else {
-      // count nelygu nuliui cartas siam vartotojui egzistuoja norim prideti i carta
+      // count nelygu nuliui cartas siam vartotojui egzistuoja norim prideti i cart
+      // currentCartArr esamas krepselis dv
+      // req.body = naujas item i krepseli
       const currentCartArr = currentCart.cart;
-      currentCartArr.push(req.body);
+      const isItemInCartAlready = currentCartArr.find(
+        (ci) => ci.itemId == req.body.itemId && ci.size === req.body.size
+      );
+      if (isItemInCartAlready) {
+        // qty ++
+        isItemInCartAlready.quantity++;
+        console.log('qty++');
+      } else {
+        currentCartArr.push(req.body);
+        console.log('item push');
+      }
       await Cart.updateOne(
         { userId: req.params.userId },
         { cart: currentCartArr }
       );
       res.json({ msg: 'now in cart', currentCart });
     }
+
     // res.json('testing');
   } catch (err) {
     res.json(err);
--- Routes/itemRoutes.js
+++ Routes/itemRoutes.js
@@ -6,24 +6,24 @@ const router = express.Router();
 router.post('/api/shop/items/new', (req, res) => {
   console.log(req.body);
 
-  const newItemData = {
-    title: 'Green hat',
-    price: 99.99,
-    salePrice: 49.9,
+  // const newItemData = {
+  //   title: 'Green hat',
+  //   price: 99.99,
+  //   salePrice: 49.9,
 
-    image: 'acc_hat_01_',
-    color: 'green',
+  //   image: 'acc_hat_01_',
+  //   color: 'green',
 
-    sizeQty: [
-      { size: 'small', quantity: 10 },
-      { size: 'medium', quantity: 10 },
-      { size: 'large', quantity: 10 },
-    ],
-    images: [1, 2, 3, 4, 5],
-    sku: 'hat_01',
-    category: '60e593ada7aa681d4846ad99',
-  };
-  const newItem = new shopItem(newItemData);
+  //   sizeQty: [
+  //     { size: 'small', quantity: 10 },
+  //     { size: 'medium', quantity: 10 },
+  //     { size: 'large', quantity: 10 },
+  //   ],
+  //   images: [1, 2, 3, 4, 5],
+  //   sku: 'hat_01',
+  //   category: '60e593ada7aa681d4846ad99',
+  // };
+  const newItem = new shopItem(req.body);
 
   newItem
     .save()
