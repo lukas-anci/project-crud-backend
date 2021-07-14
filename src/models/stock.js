abd6d8ee8bb777854141cd31d36a3d6f944d46f1
Lukas <lukas.ancikevicius@gmail.com>
2021-07-14 09:05:05 +0300

    use stock js to create item

---
+++ src/models/stock.js
@@ -0,0 +1,66 @@
+const newItemData = [
+  {
+    title: 'Autum best',
+    price: 299.99,
+    image: 'foot_autum_01_',
+    color: 'green',
+    size: 'normal',
+
+    quantity: 3,
+    images: [1, 2, 3, 4],
+    sku: 'autum_01',
+    category: '60e593bea7aa681d4846ad9d',
+  },
+  {
+    title: 'Rocco Flat Peak Cap - Fatigue',
+    price: 29.99,
+    image: 'acc_hat_01_',
+    color: 'green',
+    size: 'normal',
+
+    quantity: 10,
+    images: [1, 2, 3, 4],
+    sku: 'hat_01',
+    category: '60e593ada7aa681d4846ad99',
+  },
+  {
+    title: 'Sergeant Baseball Cap',
+    price: 59,
+    salePrice: 29.99,
+    image: 'acc_hat_02_',
+    color: 'green',
+    size: 'normal',
+
+    quantity: 15,
+    images: [1, 2, 3, 4],
+    sku: 'hat_02',
+    category: '60e593ada7aa681d4846ad99',
+  },
+  {
+    title: 'Stealth Bomber Jacket - Navy',
+    price: 99.99,
+    salePrice: 79.99,
+    image: 'acc_jacket_01_',
+    color: 'blue',
+
+    size: 'large',
+    quantity: 12,
+    images: [1, 2, 3, 4],
+    sku: 'jacket_01',
+    category: '60e593cda7aa681d4846ada1',
+  },
+  {
+    title: 'Trench Biker Jeans',
+    price: 111.99,
+    image: 'denim_jeans_01_',
+    color: 'blue',
+
+    size: 'large',
+    quantity: 8,
+    images: [1, 2, 3],
+    sku: 'jeans_01',
+    category: '60e593c5a7aa681d4846ad9f',
+  },
+];
+
+module.exports = newItemData;
